5895a24c9f44ee8b0de88c97bd49c391a81b88ca
feudal <calmis87@gmail.com>
2021-10-28 10:39:50 +0300

    Reset repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import React from "react";
 import "./App.css";
-import Header from "./componests/Header/Header";
-import { Redirect, Route, Switch } from "react-router-dom";
-import Users from "./page/Users";
-import Posts from "./page/Posts";
-import Login from "./page/Login";
-import EditPost from "./page/EditPost";
 
 function App() {
-  const user: string | null = localStorage.user;
-
-  return (
-    <div>
-      <Header />
-      <div className="container">
-        <Switch>
-          <Route path="/" exact>
-            <Login />
-          </Route>
-          {user && (
-            <>
-              <Route path="/users">
-                <Users />
-              </Route>
-              <Route path="/posts">
-                <Posts />
-              </Route>
-              <Route path="/edit-post">
-                <EditPost />
-              </Route>
-            </>
-          )}
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  );
+  return <div className="container">Hello</div>;
 }
 
 export default App;
--- src/api/users/users.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { UserInterface } from './../../interface/interface';
-
-
-export const fetchUsers = async () => {
-  const data = await axios.get<UserInterface[]>("http://localhost:3001/users");
-  return data;
-};
